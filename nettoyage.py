4eb33f32c2d6521349513dd104eb7121bf06f892
Tsari Sila <developpement0123@gmail.com>
2025-12-06 00:08:20 +0100

    Final

--- nettoyage.py
+++ nettoyage.py
@@ -3,6 +3,9 @@ import pandas
 df = pandas.read_csv('original dataset/movie_dataset.csv')
 df = df.drop(columns=['index','homepage', 'id', 'keywords', 'overview', 'release_date', 'status', 'tagline', 'title', 'original_title', 'crew'])
 
+#Supprimer les instances en duplication
+df = df.drop_duplicates()
+
 output_file = 'phases de traitement dataset/movie_dataset_cleaned1.csv'
 df.to_csv(output_file, index=False)
 
